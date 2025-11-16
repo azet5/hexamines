0a06dbf004c94258bcbbda1ff1953263dae78654
azet5 <51453773+dyzio6684@users.noreply.github.com>
2025-11-16 22:24:20 +0100

    Add SDL2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::time::Duration;
+
+use sdl2::{event::Event, pixels::Color};
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = sdl2::init().unwrap();
+    let video = sdl.video().unwrap();
+
+    let window = video.window("hexamines", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0x00, 0x7f, 0xff));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'event: loop {
+        for e in event_pump.poll_iter() {
+            match e {
+                Event::Quit { .. } => break 'event,
+                _ => {},
+            }
+        }
+
+        canvas.present();
+        std::thread::sleep(Duration::new(0, 1000000000 / 60));
+    }
 }
